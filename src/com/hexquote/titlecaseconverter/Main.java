91be6f2d2a206704c1dc0f4841d2ef92400a51d6
jawad.shani@hitachi-eu.com <jawad.shani@hitachi-eu.com>
2020-03-09 09:45:06 +0100

    TitleCaseConverter

---
+++ src/com/hexquote/titlecaseconverter/Main.java
@@ -0,0 +1,11 @@
+package com.hexquote.titlecaseconverter;
+
+public class Main {
+
+    public static void main(String args[]){
+
+        var input = "hello to the world!";
+        System.out.println(TitleCaseConverter.convertToTitleCase(input));
+
+    }
+}
